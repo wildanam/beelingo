47f276456162855ce317cfa150db71123e27bec8
wildanam <ahmadwildan.am@gmail.com>
2025-05-08 12:48:07 +0700

    update: ui for screens

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,29 @@
+import { IoSend } from "react-icons/io5";
+
 export default function Home() {
   return (
-    <section className="min-h-screen flex flex-col gap-8 justify-center items-center px-2">
-      <div className="max-w-[780px] text-center space-y-8">
+    <section className="min-h-screen flex flex-col gap-6 md:gap-8 justify-center items-center px-4">
+      <div className="max-w-[780px] text-center space-y-6 md:space-y-8">
         <div className="bg-[#1d2023] w-fit mx-auto p-2.5 rounded-full">
-          <p className="text-sm"><span className="bg-yellow-600 text-yellow-950 font-extrabold px-2 py-1 rounded-full">now</span>&nbsp;&nbsp;<span className="text-yellow-500">can only translate from english to indonesian</span></p>
+          <p className="text-[0.625rem] md:text-sm"><span className="bg-yellow-600 text-yellow-950 font-extrabold px-2 py-1 rounded-full">now</span>&nbsp;&nbsp;<span className="text-yellow-500">can only translate from english to indonesian</span></p>
         </div>
-        <h1 className="text-4xl md:text-7xl font-medium">WhatsApp Document<br /> Translator</h1>
-        <p className="leading-relaxed">Beelingo is a fast and reliable WhatsApp bot that helps you translate documents (PDF, DOCX) across languages in just a few seconds. Like a busy bee, it works tirelessly to deliver smooth, accurate translations right to your chat.</p>
+        <h1 className="text-[26px] md:text-7xl font-extrabold md:font-medium">WhatsApp Document<br /> Translator</h1>
+        <div className="max-sm:max-w-[425px]">
+          <p className="text-xs md:text-base leading-relaxed">Beelingo is a fast and reliable WhatsApp bot that helps you translate documents (PDF, DOCX) across languages in just a few seconds. Like a busy bee, it works tirelessly to deliver smooth, accurate translations right to your chat.</p>
+        </div>
       </div>
       <div className="translate-container">
-        <div className="flex gap-10 md:gap-40 items-center">
+        <div className="flex gap-20 md:gap-40 items-center">
           <p className="text-gray-500">your_document.pdf</p>
-          <button className="hero-btn">Translate</button>
+          <button className="hero-btn flex items-center gap-2">
+            {/* Icon hanya muncul di small screen */}
+            <span className="md:hidden">
+              <IoSend />
+            </span>
+
+            {/* Text hanya muncul di medium screen ke atas */}
+            <span className="hidden md:inline">Translate</span>
+          </button>
         </div>
       </div>
     </section>
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import { FaGithub } from "react-icons/fa"
 
 const Header = () => {
   return (
-    <header className="absolute w-full py-8 lg:py-2 px-8">
+    <header className="absolute w-full py-8 lg:py-2 px-4 md:px-8">
       <div className="max-w-[1080px] mx-auto flex justify-between items-center">
         <div>
           <span>Beelingo</span>
